d3de9dfa22a996210988e673c45952c3bfca78b1
Tom Zhang <tomzhang@toms-mbp.home>
2023-08-09 01:47:44 -0400

    1

--- gfinance/stock.py
+++ gfinance/stock.py
@@ -2,13 +2,20 @@ from openpyxl import load_workbook, Workbook
 from openpyxl.styles import PatternFill
 from os.path import exists
 
+
 def write_into_xl(input_file_path, xl_path):
     with open(input_file_path, "r") as file:
         rows = file.readlines()
     new_rows = []
     new_rows.append(["time", rows.pop(0)])
     categories = ["market index", "most active", "most gainers", "most losers"]
-    col_types = ["rank", "stock name", "curr price", "change percentage today", "changed price today"]
+    col_types = [
+        "rank",
+        "stock name",
+        "curr price",
+        "change percentage today",
+        "changed price today",
+    ]
     category = 0
     for i in rows:
         if i[:3] == " 1?":
@@ -32,8 +39,6 @@ def write_into_xl(input_file_path, xl_path):
     color = "000000"
     fill = PatternFill(start_color=color, end_color=color, fill_type="solid")
     for col in range(1, 6):
-        cell = ws.cell(row=len(new_rows) + 1, column=col)
+        cell = ws.cell(row=len(new_rows), column=col)
         cell.fill = fill
     wb.save(xl_path)
-
-write_into_xl("stocks/data.txt", "stocks/output.xlsx")
